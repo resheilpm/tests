d6ca2b1f27dcb3a4032602366020cc7bc521c11e
resheilSNT <resheil.payyadi@stayntouch.com>
2023-08-01 07:57:52 -0400

    Add third vulnerability

--- src/script.js
+++ src/script.js
@@ -1,3 +1,6 @@
 // Vulnerable code
 const userInput = '<script>alert("XSS attack!")</script>';
 document.getElementById("output").innerHTML = userInput;
+
+// Third vulnerability
+var AWS_TOKEN = "1234567890";
