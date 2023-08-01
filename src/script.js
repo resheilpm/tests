e86a27830dec3ece61f6e9bea3952b920b517d97
resheilSNT <resheil.payyadi@stayntouch.com>
2023-08-01 08:28:46 -0400

    test

--- src/script.js
+++ src/script.js
@@ -5,7 +5,34 @@ document.getElementById("output").innerHTML = userInput;
 // Third vulnerability
 var AWS_TOKEN = "XX648ssse";
 
-
 if (5 === 5) {
   // Some code here
+}
+
+function getProp(object, propertyName, defaultValue) {
+  if (!object[propertyName]) {
+    return defaultValue;
+  }
+  return object[propertyName];
+}
+
+const hero = {
+  name: "Batman",
+  isVillian: false,
+};
+
+console.log(getProp(hero, "name", "Unknown")); // => 'Batman'
+
+function someFunc(array, array) {
+  var index,
+    item,
+    length = array.length;
+  /*
+   * Lots of code
+   */
+  for (index = 0; index < length; index++) {
+    item = array[index];
+    // Use `item`
+  }
+  return someResult;
 }
